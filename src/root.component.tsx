0bbc7c2aac083afcbbe09edd080f5ad0e7d7eb6c
Corwin Marsh <corwinm@users.noreply.github.com>
2022-05-09 13:14:14 -0700

    Add Bulma Navbar and example links

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,3 +1,32 @@
+import { Navbar } from "react-bulma-components";
+import { BrowserRouter, Link } from "react-router-dom";
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return (
+    <BrowserRouter>
+      <Navbar color="black">
+        <Navbar.Brand>
+          <Navbar.Item renderAs={Link} to="">
+            Example App
+          </Navbar.Item>
+          <Navbar.Burger />
+        </Navbar.Brand>
+        <Navbar.Menu>
+          <Navbar.Container>
+            <Navbar.Link arrowless renderAs={Link} to="/">
+              Home
+            </Navbar.Link>
+            <Navbar.Link arrowless renderAs={Link} to="/search">
+              Search
+            </Navbar.Link>
+          </Navbar.Container>
+          <Navbar.Container align="right">
+            <Navbar.Link arrowless renderAs={Link} to="/settings">
+              Settings
+            </Navbar.Link>
+          </Navbar.Container>
+        </Navbar.Menu>
+      </Navbar>
+    </BrowserRouter>
+  );
 }
